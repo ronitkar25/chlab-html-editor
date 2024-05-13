5823d95aea6391979d3ea73e585f60709bc80695
ronitkar25 <rok73@pitt.edu>
2024-05-13 15:36:05 -0400

    Update editor.js

--- editor.js
+++ editor.js
@@ -1,4 +1,5 @@
 document.getElementById('startButton').addEventListener('click', async () => {
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
     const urls = [
         'https://chlabtest.webflow.io/index.html',
         'https://chlabtest.webflow.io/research.html',
@@ -12,34 +13,38 @@ document.getElementById('startButton').addEventListener('click', async () => {
         'https://chlabtest.webflow.io/ech.html'
     ];
 
-    let files = await Promise.all(urls.map(url => fetch(url).then(response => response.text())));
+    try {
+        let files = await Promise.all(urls.map(url => fetch(corsProxy + url).then(response => response.text())));
 
-    const zip = new JSZip();
-    const folderName = "CHLab_Edited";
+        const zip = new JSZip();
+        const folderName = "CHLab_Edited";
 
-    urls.forEach((url, index) => {
-        let text = files[index];
-        text = text.replace(/href="\//g, 'href="');
-        text = text.replace(/href="\/research"/g, 'href="research.html"');
-        text = text.replace(/href="\/publications"/g, 'href="publications.html"');
-        text = text.replace(/href="\/news"/g, 'href="news.html"');
-        text = text.replace(/href="\/outreach"/g, 'href="outreach.html"');
-        text = text.replace(/href="\/members"/g, 'href="members.html"');
-        text = text.replace(/href="\/contact"/g, 'href="contact.html"');
-        text = text.replace(/href="\/biomaterial-chemistry"/g, 'href="biomaterial-chemistry.html"');
-        text = text.replace(/href="\/scaffold-fabrication"/g, 'href="scaffold-fabrication.html"');
-        text = text.replace(/href="\/ech"/g, 'href="ech.html"');
+        urls.forEach((url, index) => {
+            let text = files[index];
+            text = text.replace(/href="\//g, 'href="');
+            text = text.replace(/href="\/research"/g, 'href="research.html"');
+            text = text.replace(/href="\/publications"/g, 'href="publications.html"');
+            text = text.replace(/href="\/news"/g, 'href="news.html"');
+            text = text.replace(/href="\/outreach"/g, 'href="outreach.html"');
+            text = text.replace(/href="\/members"/g, 'href="members.html"');
+            text = text.replace(/href="\/contact"/g, 'href="contact.html"');
+            text = text.replace(/href="\/biomaterial-chemistry"/g, 'href="biomaterial-chemistry.html"');
+            text = text.replace(/href="\/scaffold-fabrication"/g, 'href="scaffold-fabrication.html"');
+            text = text.replace(/href="\/ech"/g, 'href="ech.html"');
 
-        let fileName = url.split('/').pop();
-        zip.folder(folderName).file(fileName, text);
-    });
-
-    zip.generateAsync({type:"blob"})
-        .then(function(content) {
-            const downloadLink = document.getElementById('downloadLink');
-            downloadLink.href = URL.createObjectURL(content);
-            downloadLink.download = `${folderName}.zip`;
-            downloadLink.style.display = 'block';
-            downloadLink.click();
+            let fileName = url.split('/').pop();
+            zip.folder(folderName).file(fileName, text);
         });
+
+        zip.generateAsync({ type: "blob" })
+            .then(function (content) {
+                const downloadLink = document.getElementById('downloadLink');
+                downloadLink.href = URL.createObjectURL(content);
+                downloadLink.download = `${folderName}.zip`;
+                downloadLink.style.display = 'block';
+                downloadLink.click();
+            });
+    } catch (error) {
+        console.error('Error fetching or processing files:', error);
+    }
 });
